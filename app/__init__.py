4c60482fa00d61a4b59712b6269fccda901c19aa
yhy666888 <1921569424@qq.com>
2020-11-20 17:53:37 +0800

    1120更新-完善电影内容功能 新增添加电影预告功能、预告列表

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
+import os
+import uuid
+import datetime
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,6 +10,8 @@ app.debug = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://root:password@127.0.0.1:3306/movie"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 app.config['SECRET_KEY'] = 'dad9c4cbb6a449cc8436b7b78e077341'
+# 定义文件上传保存的路径，在__init__.py文件所在目录创建media文件夹，用于保存上传的文件
+app.config['UP_DIR'] = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static/media/')
 
 # 定义db对象，实例化SQLAlchemy，传入app对象
 db = SQLAlchemy(app)
